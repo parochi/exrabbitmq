a64fb40a869e75d00ae42aea71ca8706487350ed
Rajeshkumar Reddy <55693953+technitos@users.noreply.github.com>
2020-05-28 19:52:13 -0700

    Consumer to consume messages

---
+++ consumer.sh
@@ -0,0 +1,21 @@
+const amqp = require("amqplib");
+
+connect();
+async function connect(){
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672")
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        
+        channel.consume("jobs", message => {
+            console.log(message.content.toString());
+            channel.ack(message)
+        })
+
+        console.log("Waiting for messages...")
+
+    }
+    catch(ex){
+        console.error(ex)
+    }
+}
